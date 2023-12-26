3e17eeaa8cea8707cf3865bed643565e2ed4ae85
BFILMA <leyakat.organa@gmail.com>
2023-12-26 23:51:00 +0530

    inheritance

---
+++ Subclass.java
@@ -0,0 +1,6 @@
+public class Subclass extends Superclass{
+    public static void main(String[] args) {
+        Subclass obj = new Subclass();
+        obj.m1();
+    }
+}
---
+++ Superclass.java
@@ -0,0 +1,5 @@
+public class Superclass {
+    public void m1(){
+        System.out.println("Method of superclass - inherited");
+    }
+}
