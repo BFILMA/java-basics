1d1a273249f7cb4e6e7c7de17f8d20a2ee5e19fe
BFILMA <leyakat.organa@gmail.com>
2023-12-26 18:30:30 +0530

    exception

---
+++ ExceptionHandling.java
@@ -0,0 +1,11 @@
+public class ExceptionHandling{
+    public static void main(String[] args) {
+        try{
+            int data = 100/0;
+        }
+        catch(ArithmeticException e){
+            System.out.println(e);
+        }
+        System.out.println("Rest of the code will execute");
+    }
+}
