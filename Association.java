bdc2d00dbfa3a5b83cb1b1be54957e2fb35f137b
BFILMA <leyakat.organa@gmail.com>
2024-01-14 18:34:45 +0530

    association

---
+++ Association.java
@@ -0,0 +1,7 @@
+public class Association {
+    public static void main(String[] args) {
+        Bank b = new Bank();
+        Employee e = new Employee();
+        b.setEmployee(e);
+    }
+}
---
+++ Bank.java
@@ -0,0 +1,9 @@
+import java.util.Set;
+
+public class Bank {
+    private Set<Employee> employees;
+
+    public void setEmployee(Employee e){
+        employees.add(e);
+    }
+}
